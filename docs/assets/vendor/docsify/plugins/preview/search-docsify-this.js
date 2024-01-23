89ab1feaf01178a7eb983077c74779065c88d6ea
Paul Hibbitts <paul@paulhibbitts.com>
2024-01-23 09:24:21 -0800

    Update search-docsify-this.js

--- docs/assets/vendor/docsify/plugins/preview/search-docsify-this.js
+++ docs/assets/vendor/docsify/plugins/preview/search-docsify-this.js
@@ -242,6 +242,14 @@
       const postContent = post.body && post.body.trim();
       const postUrl = post.slug || '';
 
+      // Skip posts that contain iframes, Font Awesome icons, embedly cards, or Markdown images
+      // console.log(postContent);
+      const isImage = /!\[[^\]]*\]\([^)]*\)/g.test(postContent); // Check if it's a Markdown image
+
+      if (postContent.includes('iframe') || postContent.includes(':fas') || postContent.includes(':fab') || postContent.includes('embedly-card') || isImage) {
+        continue;
+      }
+
       if (postTitle) {
         keywords.forEach(keyword => {
           // From https://github.com/sindresorhus/escape-string-regexp
@@ -347,7 +355,7 @@
 
     const isExpired = localStorage.getItem(expireKey) < Date.now();
 
-    INDEXS = {};
+    INDEXS = JSON.parse(localStorage.getItem(indexKey));
 
     if (isExpired) {
       INDEXS = {};
